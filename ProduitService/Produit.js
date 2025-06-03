cac3608301ae2ad7b22ad3716ddc29d424675646
Mohamedtwd <superraja2003@gmail.com>
2025-06-03 09:53:08 +0100

    push

---
+++ ProduitService/Produit.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const ProduitSchema = mongoose.Schema({
+    nom: String,
+    description: String,
+    prix: Number,
+    created_at: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+module.exports = Produit = mongoose.model("produit", ProduitSchema);
+
+// 68372e46ac9943e580efeadb
---
+++ ProduitService/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT_ONE || 4000;
+const mongoose = require("mongoose");
+const Produit = require("./Produit");
+app.use(express.json());
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect(
+    "mongodb://localhost/produit-service",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    console.log(`Produit-Service DB Connected`)
+    
+);
+
+app.post("/produit/ajouter", (req, res, next) => {
+    const { nom, description, prix } = req.body;
+    const newProduit = new Produit({
+        nom,
+        description,
+        prix
+    });
+
+    newProduit.save()
+        .then(produit => res.status(201).json(produit))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.post("/produit/acheter", (req, res, next) => {
+    const { ids } = req.body;
+    Produit.find({ _id: { $in: ids } })
+        .then(produits => res.status(201).json(produits))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.listen(PORT, () => {
+    console.log(`Product-Service at ${PORT}`);
+});
+
